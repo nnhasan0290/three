ab5df6b386c72db6e63999f2d00af2195fab1c14
Nazmul Hasan <nazmulhasan02901@gmail.com>
2023-02-04 23:59:25 +0000

    added orbit control and circle, plane , grid helper

--- threejs/main.js
+++ threejs/main.js
@@ -1,18 +1,59 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(30, 1);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(500, 300);
+renderer.setSize(window.innerWidth, window.innerHeight);
 // document.body.appendChild(renderer.domElement);
-document.getElementById("app").appendChild(renderer.domElement)
+document.getElementById("app").appendChild(renderer.domElement);
 
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+//first scence
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.z = 10;
-camera.position.x = -1;
+//second scene
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
 
-renderer.render(scene, camera);
+//third scene
+const planeGeometry = new THREE.PlaneGeometry(20, 20);
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+
+//forth scene
+const gridHelper = new THREE.GridHelper(20);
+scene.add(gridHelper);
+
+//fifth scene
+const sphereGeometry = new THREE.SphereGeometry(2,25,25);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000FF,
+    wireframe: false
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+sphere.position.set(-10, 10, 0)
+
+camera.position.set(-10, 30, 30);
+
+orbit.update();
+
+function animate(time) {
+  box.rotation.x = time / 1000;
+  box.rotation.y = time / 1000;
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
